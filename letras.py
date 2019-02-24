0bfaedc1077826be77b82ae40176332f07e491ad
JL Granda Rasp <jorge_granda82112@elpoli.edu.co>
2019-02-23 19:06:18 -0500

    no

--- letras.py
+++ letras.py
@@ -6,27 +6,27 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-# DISPLAY NRO 1
-D1a =  # letras del display
-D1b =  # letras del display
-D1c =  # letras del display
-D1d =  # letras del display
-D1e =  # letras del display
-D1f =  # letras del display
-D1g =  # letras del display
-
+# PINES DISPLAY NRO 1
+D1a = 16  # letras del display
+D1b = 12  # letras del display
+D1c = 13  # letras del display
+D1d = 19  # letras del display
+D1e = 26  # letras del display
+D1f = 20  # letras del display
+D1g = 21  # letras del display
+D1dp = 6
 # DISPLAY NRO 2
-D2a = # letras del display 
-D2b = # letras del display
-D2c = # letras del display
-D2d = # letras del display
-D2e = # letras del display
-D2f = # letras del display
-D2g = # letras del display
+D2a = 25  # letras del display 
+D2b = 24 # letras del display
+D2c = 9 #letras del display
+D2d = 11 # letras del display
+D2e = 5 # letras del display
+D2f = 8 #letras del display
+D2g = 7 # letras del display
+D2dp = 10
 
-
-display1 = [D1a, D1b, D1c,D1d,D1e,D1f,D1g]
-display2 = [D2a, D2b, D2c, D2d, D2e, D2f, D2g ]
+display1 = [D1a, D1b, D1c, D1d, D1e, D1f, D1g, D1dp]
+display2 = [D2a, D2b, D2c, D2d, D2e, D2f, D2g, D2dp]
 
 
 def clean():
@@ -39,3 +39,13 @@ def setup_pin():
 
 def leds_off():
     GPIO.output([display1, display2], GPIO.LOW)
+
+
+setup_pin()
+GPIO.output(display2, True)
+GPIO.output(display1, True)
+sleep(3)
+clean()
+
+
+
